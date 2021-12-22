cc6476c55ca0817f9b36b813e4df8992abb153a5
Josefine Löfgren <josefinelofgren@Josefines-MacBook-Pro.local>
2021-12-22 14:12:26 +0100

    create and add navbar component

--- design/src/App.js
+++ design/src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header/Header.jsx';
+import Navbar from './components/Navbar/Navbar.jsx';
 import './App.scss';
 
 function App() {
     return (
         <div className="App">
+            <Navbar /> 
             <Header />
         </div>
     );
