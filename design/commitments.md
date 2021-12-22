1ba2021370390794df8e1454fa682a13281396c0
Mats haby <mats.haby@live.com>
2021-12-22 10:08:50 +0100

    docs: :memo: Add file commitments.md that will contain our work agreements

---
+++ design/commitments.md
@@ -0,0 +1,7 @@
+# Lemon drops commitments
+
+-   Alla jobbar fria tider
+-   Stämma av i teams varannan dag hur vi ligger till (Måndag/Onsdag/Fredag)
+-   Man säger till i chatten när det finns en PR att reviewa så reviewar man så fort man har tid och möjlighet.
+-   Två personer från vår grupp ska reviewa och kommentera/ge feedback på ens PR
+-   Deadline för issues den 2 januari 23.59. Veckan efter det går vi igenom och ser så att allt är klart
--- design/src/App.js
+++ design/src/App.js
@@ -1,10 +1,19 @@
+import React, { useState } from 'react';
 import Header from './components/Header/Header.jsx';
+import Cookie from './components/Cookie/Cookie.jsx';
 import './App.scss';
 
 function App() {
+    const [visibleCookie, setVisibleCookie] = useState(true);
     return (
         <div className="App">
             <Header />
+            {visibleCookie && (
+                <Cookie
+                    visibleCookie={visibleCookie}
+                    setVisibleCookie={setVisibleCookie}
+                />
+            )}
         </div>
     );
 }
