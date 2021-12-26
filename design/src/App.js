dfc90a7b7ac69b76a68451ff072b87572e13fa30
dmlfahfn <dmlfahfn1@hotmail.se>
2021-12-26 23:14:38 +0100

    somethings...

--- design/src/App.js
+++ design/src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header/Header.jsx';
+import Glogg from './components/Glogg/Glogg.jsx';
 import './App.scss';
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <Glogg />
         </div>
     );
 }
