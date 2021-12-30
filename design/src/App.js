8516444a4e612e6249f368143b243657b7a47955
dmlfahfn <dmlfahfn1@hotmail.se>
2021-12-30 13:53:44 +0100

    ready for merge, i think

--- design/src/App.js
+++ design/src/App.js
@@ -1,12 +1,24 @@
+import React, { useState } from 'react';
 import Header from './components/Header/Header.jsx';
 import Glogg from './components/Glogg/Glogg.jsx';
+import Cookie from './components/Cookie/Cookie.jsx';
+
 import './App.scss';
 
 function App() {
+
+    const [visibleCookie, setVisibleCookie] = useState(true);
+
     return (
         <div className="App">
             <Header />
             <Glogg />
+            {visibleCookie && (
+                <Cookie
+                    visibleCookie={visibleCookie}
+                    setVisibleCookie={setVisibleCookie}
+                />
+            )}
         </div>
     );
 }
