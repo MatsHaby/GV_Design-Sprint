3b48d3a98b1d6792220b543d6eac7b9952658f4f
Igor Milosevic <igor.milosevic1988@gmail.com>
2021-12-31 14:40:56 +0100

    added Footer components, added styling to form component

--- design/src/App.js
+++ design/src/App.js
@@ -3,20 +3,21 @@ import Header from './components/Header/Header.jsx';
 import Cookie from './components/Cookie/Cookie.jsx';
 import './App.scss';
 import Form from './components/Form.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
     const [visibleCookie, setVisibleCookie] = useState(true);
 
     return (
         <div className="App">
-            <Header />
+            {/* <Header />
             {visibleCookie && (
                 <Cookie
                     visibleCookie={visibleCookie}
                     setVisibleCookie={setVisibleCookie}
                 />
-            )}
-
+            )} */}
+            <Form />
         </div>
     );
 }
--- design/src/components/Form.jsx
+++ design/src/components/Form.jsx
@@ -9,15 +9,17 @@ const Form = () => {
  const [phone, setPhone] = useState('');
 
  return (
-  <div>
-   <div>Beställ smakprov på årets glögg</div>
-   <form>
-    <input type="text" placeholder='Namn' id="name" value={name} onChange={(e) => setName(e.target.value)} />
-    <input type="text" placeholder='E-mail' id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-    <input type="text" placeholder='Adress' id="address" value={address} onChange={(e) => setAddress(e.target.value)} />
-    <input type="text" placeholder='Telefonnummer' id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
-   </form>
-   <button id="order-btn">Beställ</button>
+  <div className="hero-background">
+   <div className='hero-igor'>
+    <h3>Beställ smakprov på årets glögg</h3>
+    <form>
+     <input className='inputs' type="text" placeholder='Namn' id="name" value={name} onChange={(e) => setName(e.target.value)} /><br />
+     <input className='inputs' type="text" placeholder='E-mail' id="email" value={email} onChange={(e) => setEmail(e.target.value)} /><br />
+     <input className='inputs' type="text" placeholder='Adress' id="address" value={address} onChange={(e) => setAddress(e.target.value)} /><br />
+     <input className='inputs' type="text" placeholder='Telefonnummer' id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} /><br />
+    </form>
+    <button id="order-btn">Beställ</button>
+   </div>
   </div>
  )
 }
