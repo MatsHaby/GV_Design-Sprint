ba2b6699273262b518d246ee321579e8062ad8c3
Igor Milosevic <igor.milosevic1988@gmail.com>
2022-01-07 10:50:15 +0100

    finalized form and footer feature

--- design/src/components/Footer.jsx
+++ design/src/components/Footer.jsx
@@ -15,16 +15,16 @@ const Footer = () => {
     <p>010-11 22 333<br />kundservice@glogga.se</p>
    </div>
    <div className="footer-item second">
-    <a href="www.omoss.se">Om oss</a>
-    <a href="www.jobba.se">Jobba här</a>
-    <a href="www.vilkor.se">Köpvilkor</a>
+    <a href="#">Om oss</a>
+    <a href="#">Jobba här</a>
+    <a href="#">Köpvilkor</a>
     <a href="#">Faq</a>
    </div>
    <div className="footer-item third">
-    <a href="#"><img src={Instagram} alt="Besök vår Instagram sida" /></a>
-    <a href="#"><img src={Facebook} alt="Besök vår Facebook sida" /></a>
-    <a href="#"><img src={Twitter} alt="Besök vår Twitter sida" /></a>
-    <a href="#"><img src={Youtube} alt="Besök vår Youtube kanal" /></a>
+    <a href="#"><img src={Instagram} alt="Besök vår Instagram-sida" /></a>
+    <a href="#"><img src={Facebook} alt="Besök vår Facebook-sida" /></a>
+    <a href="#"><img src={Twitter} alt="Besök vår Twitter-sida" /></a>
+    <a href="#"><img src={Youtube} alt="Besök vår Youtube-kanal" /></a>
    </div>
   </footer>
  )
